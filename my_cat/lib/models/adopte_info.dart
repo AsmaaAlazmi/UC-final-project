e9fcc69178f7d76c07db24f12db34f33ad5055ac
Ahmad AlAmiri <54888344+ironik96@users.noreply.github.com>
2022-09-27 23:47:09 +0300

    ahmad edits

---
+++ my_cat/lib/models/adopte_info.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+class Information {
+  String animal;
+  String gender;
+  String age;
+  String vac;
+  File? small_imageURL;
+  File? big_imageURL;
+  File? image1;
+  File? image2;
+  String rules;
+  String breed;
+
+  String phone;
+
+  Information(
+      {required this.animal,
+      required this.gender,
+      required this.age,
+      required this.vac,
+      required this.small_imageURL,
+      required this.big_imageURL,
+      required this.breed,
+      required this.image1,
+      required this.image2,
+      required this.rules,
+      required this.phone});
+}
---
+++ my_cat/lib/screens/home_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:my_cat/main.dart';
+
+import '../models/adopte_info.dart';
+import 'addanimal_screen.dart';
+import 'adopte_screen.dart';
+
+class Home extends StatelessWidget {
+  Home({super.key});
+
+  List<Information> cat = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: <Widget>[
+      //VideoWidget(),
+      Scaffold(
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+          backgroundColor: Colors.transparent,
+          body: Container(
+            color: Colors.transparent,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.transparent,
+                  padding: EdgeInsets.only(top: 300),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.transparent,
+                      padding: EdgeInsets.only(top: 10),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Adopte(
+                                      catList: cat,
+                                    )));
+                      },
+                      child: Text(
+                        'Adopte',
+                        style: TextStyle(color: Colors.white, fontSize: 17),
+                      ),
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all<EdgeInsets>(
+                          EdgeInsets.all(15),
+                        ),
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                            Colors.transparent),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(color: Colors.white))),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  color: Colors.transparent,
+                  padding: EdgeInsets.only(top: 65),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Add(
+                                      cat: cat,
+                                    )));
+                      },
+                      child: Text(
+                        'Place For Adoption',
+                        style: TextStyle(color: Colors.white, fontSize: 17),
+                      ),
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all<EdgeInsets>(
+                          EdgeInsets.all(15),
+                        ),
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                            Colors.transparent),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(color: Colors.white))),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ))
+    ]);
+  }
+}
